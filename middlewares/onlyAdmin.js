3c9fabb48bb7cf1246d3de1c0c88faeb59050ed5
Santiago Graviano <santiago.graviano@gmail.com>
2022-03-19 17:41:00 -0300

    Middleware name changed

--- middlewares/onlyAdmin.js
+++ middlewares/onlyAdmin.js
@@ -2,10 +2,10 @@ import 'dotenv/config'
 
 const isAdmin = process.env.IS_ADMIN == 'true' ? true : false
 
-const isAdminMiddleware = (req, res, next) => {
+const onlyAdmin = (req, res, next) => {
   if (!isAdmin)
     res.json({ error: -1, description: `Ruta ${ req.originalUrl } método ${ req.method } no autorizada` })
   else next()
 }
 
-export default isAdminMiddleware
+export default onlyAdmin
